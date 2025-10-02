967a1a19f79dd8a2e8730b29c0780fa9c34173ed
nourhene <benothmennourhen8@gmail.com>
2025-10-02 16:19:18 +0100

    ✨ Update User model (timestamps or other changes)

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const userSchema=mongoose.Schema({
+const userSchema=new mongoose.Schema({
     name: String,
   email: { type: String, unique: true },
   password: String,
