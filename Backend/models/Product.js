bc5e8c2565a0a1081e35fde8f0fc76472f82d84e
nourhene <benothmennourhen8@gmail.com>
2025-10-04 21:54:23 +0100

    📦 Update Product model, server config & dependencies

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -5,6 +5,12 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true }, // product name
   price: { type: Number, required: true }, // product price
   stock: { type: Number, default: 0 }, // available stock
+   images: [
+    {
+      url: String,       // URL de l'image sur Cloudinary
+      public_id: String, // ID pour pouvoir supprimer l'image si nécessaire
+    },
+  ],
   // Each product belongs to a single category
   category: { 
     type: mongoose.Schema.Types.ObjectId, 
--- Backend/server.js
+++ Backend/server.js
@@ -14,10 +14,13 @@ const app = express();
 // Middleware to parse JSON
 app.use(express.json());
 
-// Test route
+
+
+// Test route publique
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
 
 // Set the port
 const PORT = process.env.PORT || 3000;
